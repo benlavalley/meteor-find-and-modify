c9bcbda49729799c0ef9be802062ef6128a52052
Ben Lavalley <ben@nibbl.com>
2021-10-28 23:20:04 -0400

    compatibility update

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-	api.versionsFrom('1.8.1');
+	api.versionsFrom('2.5');
 	api.use('ecmascript', ['client', 'server']);
 	api.use('ddp', ['client', 'server']);
 	api.use('mongo', ['client', 'server']);
